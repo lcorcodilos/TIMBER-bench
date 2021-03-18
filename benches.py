fa0ebc6c626d5399bff3973a869b027a44114bbe
Lucas Corcodilos <lcorcodi@cern.ch>
2021-03-18 13:18:33 -0500

    Switch TIMBER bench to AutoJME

--- benches.py
+++ benches.py
@@ -1,3 +1,4 @@
+from TIMBER.Tools.AutoJME import AutoJME
 from memory_profiler import profile
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import * 
 from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 import *#UncertaintiesFactorized import *
@@ -84,34 +85,7 @@ class BenchTIMBER(Bench):
         if cutstring != '':
             self.a.Cut("cutstring",cutstring)
 
-        if not self.a.isData:
-            jes = Calibration("JES","TIMBER/Framework/include/JES_weight.h",
-                    [GetJMETag("JES",str(year),"MC"),"AK8PFPuppi","","true"], corrtype="Calibration")
-            jer = Calibration("JER","TIMBER/Framework/include/JER_weight.h",
-                    [GetJMETag("JER",str(year),"MC"),"AK8PFPuppi"], corrtype="Calibration")
-            jms = Calibration("JMS","TIMBER/Framework/include/JMS_weight.h",
-                    [str(year)], corrtype="Calibration")
-            jmr = Calibration("JMR","TIMBER/Framework/include/JMR_weight.h",
-                    [str(year)], corrtype="Calibration")
-        else:
-            jes = Calibration("JES","TIMBER/Framework/include/JES_weight.h",
-                    [GetJMETag("JES",str(year),"C"),"AK8PFPuppi","","true"], corrtype="Calibration")
-
-        if not self.a.isData:
-            calibdict = {"FatJet_pt":[jes,jer],"FatJet_mass":[jes,jer,jms,jmr]}
-            evalargs = {
-                jes: {"jets":"FatJets","rho":"fixedGridRhoFastjetAll"},
-                jer: {"jets":"FatJets","genJets":"GenJetAK8s"},
-                jms: {"nJets":"nFatJet"},
-                jmr: {"jets":"FatJets","genJets":"GenJetAK8s"}
-            }
-        else:
-            calibdict = {"FatJet_pt":[jes],"FatJet_mass":[jes]}
-            evalargs = {
-                jes: {"jets":"FatJets","rho":"fixedGridRhoFastjetAll"}
-            }
-        self.a.CalibrateVars(calibdict,evalargs,"CalibratedFatJet",variationsFlag=self.isMC)
-
+        self.a = AutoJME(self.a, "FatJet", year, "C")
         self.outfilename = 'benchmark_out/TIMBER_'+self.tag+'.root'
     
     def run_timber(self):
